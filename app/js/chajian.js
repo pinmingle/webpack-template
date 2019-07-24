3f17fc26536ddacc7490df71e5a09805b9027a4e
pinmingle <taotao19871001@126.com>
2019-07-24 13:58:24 +0800

    remove app

--- app/js/chajian.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by n-158 on 2019/6/3.
- */
-module.exports = "这是测试的一段哈123"
--- app/js/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by n-158 on 2019/6/3.
- */
-
-const str = require('./chajian.js');
-import '../css/main.css';
-document.getElementById("str").innerHTML = str;
